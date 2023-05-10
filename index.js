3b284f51a07ebcfd8df94cec1f4251d7e954de91
Duy Nguyen <nguyenvanduy2286@gmail.com>
2023-05-10 10:44:51 +0700

    Push

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 
 const app = express();
-app.get("/about", (req, res) => {
-  res.send("Hello about");
+app.get("/contact", (req, res) => {
+  res.send("Hello contact");
 });
 
 app.listen(3000, () => {
